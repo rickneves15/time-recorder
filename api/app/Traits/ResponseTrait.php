620099644abc518e0dfc8de6f7721e4676f576ad
Richard Neves <ricknevesbc@icloud.com>
2023-04-17 15:09:54 -0300

    feat: add trait for return response in controller

---
+++ api/app/Traits/ResponseTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+
+trait ResponseTrait
+{
+    public function responseSuccess($data, $message = "Successful", $status_code = JsonResponse::HTTP_OK): JsonResponse
+    {
+        return response()->json($data);
+    }
+
+    public function responseError($errors, $message = 'Data is invalid', $status_code = JsonResponse::HTTP_BAD_REQUEST): JsonResponse
+    {
+        return response()->json([
+            'status'  => false,
+            'message' => $message,
+            'errors'  => $errors,
+            'data'    => null,
+        ], $status_code);
+    }
+}
